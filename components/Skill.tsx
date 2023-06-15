179f9faabf3118e24801b190cb594e1b0ff6c0a9
4orrest <76661750+4orrest@users.noreply.github.com>
2023-06-14 20:17:41 -0700

    added framer motion styling to buttons, added anchor tags linking to the documentation of all skills

--- components/Skill.tsx
+++ components/Skill.tsx
@@ -1,33 +1,90 @@
+"use client";
 import Image from "next/image";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const skills = [
-  { imgSrc: "/skills/javascript.png", altText: "JavaScript" },
-  { imgSrc: "/skills/reactjs.png", altText: "React" },
-  { imgSrc: "/skills/nextJS.svg", altText: "NextJS" },
-  { imgSrc: "/skills/mongodb.png", altText: "MongoDB" },
-  { imgSrc: "/skills/nodejs.png", altText: "NodeJS" },
-  { imgSrc: "/skills/framer-motion.svg", altText: "Framer Motion" },
-  { imgSrc: "/skills/tailwind.png", altText: "Tailwind" },
-  { imgSrc: "/skills/git.png", altText: "Git" },
-  { imgSrc: "/skills/typescript.svg", altText: "Typescript" },
-  { imgSrc: "/skills/prisma.png", altText: "Prisma" },
+  {
+    imgSrc: "/skills/javascript.png",
+    altText: "JavaScript",
+    link: "https://developer.mozilla.org/en-US/docs/Web/JavaScript",
+  },
+  {
+    imgSrc: "/skills/reactjs.png",
+    altText: "React",
+    link: "https://react.dev/",
+  },
+  {
+    imgSrc: "/skills/nextJS.svg",
+    altText: "NextJS",
+    link: "https://nextjs.org/docs",
+  },
+  {
+    imgSrc: "/skills/mongodb.png",
+    altText: "MongoDB",
+    link: "https://www.mongodb.com/docs/",
+  },
+  {
+    imgSrc: "/skills/nodejs.png",
+    altText: "NodeJS",
+    link: "https://nodejs.org/en/docs",
+  },
+  {
+    imgSrc: "/skills/framer-motion.svg",
+    altText: "Framer Motion",
+    link: "https://www.framer.com/motion/",
+  },
+  {
+    imgSrc: "/skills/tailwind.png",
+    altText: "Tailwind",
+    link: "https://v2.tailwindcss.com/docs",
+  },
+  {
+    imgSrc: "/skills/git.png",
+    altText: "Git",
+    link: "https://git-scm.com/doc",
+  },
+  {
+    imgSrc: "/skills/typescript.svg",
+    altText: "Typescript",
+    link: "https://www.typescriptlang.org/docs/",
+  },
+  {
+    imgSrc: "/skills/prisma.png",
+    altText: "Prisma",
+    link: "https://www.prisma.io/docs",
+  },
   // More skills
 ];
 
 export default function Skills() {
   return (
-    <div>
+    <div className="flex flex-wrap justify-center">
       {skills.map((skill) => (
-        <button key={skill.altText} className="text-center">
-          <Image
-            src={skill.imgSrc}
-            alt={skill.altText}
-            width={50}
-            height={50}
-            className="flex flex-wrap flex-row justify-center z-10 md:justify-start rounded-full border-solid cursor-pointer m-4"
-          />
+        <div key={skill.altText} className="flex flex-col items-center m-5">
+          {skill.link ? (
+            <a href={skill.link} target="_blank" rel="noopener noreferrer">
+              <motion.img
+                src={skill.imgSrc}
+                alt={skill.altText}
+                width={50}
+                height={50}
+                whileHover={{ scale: 1.5 }}
+                className="rounded-full border-solid cursor-pointer"
+              />
+            </a>
+          ) : (
+            <motion.img
+              src={skill.imgSrc}
+              alt={skill.altText}
+              width={50}
+              height={50}
+              whileHover={{ scale: 1.1 }}
+              className="rounded-full border-solid cursor-pointer"
+            />
+          )}
           <p className="mt-2 text-sm">{skill.altText}</p>
-        </button>
+        </div>
       ))}
     </div>
   );
